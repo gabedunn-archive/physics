aebdbc6ee16e9a29898f37174fbe7b3e6f016d6d
Gabriel Dunn <gabe.dunn@shaw.ca>
2019-04-02 11:41:56 -0600

    make plotter more dynamic

--- plotter.py
+++ plotter.py
@@ -1,17 +1,39 @@
 import matplotlib.pyplot as plt
-import json
+import json, sys
 
-with open('unit28.1.json') as json_file:
-    data = json.load(json_file)
-    xkcd = data['xkcd']
-    grid = data['grid']
-    values = data['plots']
+if len(sys.argv) < 2:
+    sys.exit('specify the unit number (eg. 28.1)')
+
+unit = sys.argv[1]
+
+file = 'unit' + unit + '.json'
+
+with open(file) as json_file:
+    unit_data = json.load(json_file)
+    title = unit_data['title']
+    xkcd = unit_data['xkcd']
+    grid = unit_data['grid']
+    plots = unit_data['plots']
 
 def plotter ():
+    if len(plots) != 1:
+        sys.exit('not one plot')
+    else:
+        n = 0
+    # set up the plot stuff
+    plt.title(title)
+    plt.subplots_adjust(left=0.15, bottom=0.15)
     # grab data from dictionary and add it to the plot
-    plt.plot(values[0]['x']['values'], values[0]['y']['values'], 'bo')
-    plt.xlabel(values[0]['x']['title'])
-    plt.ylabel(values[0]['y']['title'])
+    plot = plots[n]
+    x = plot['x']
+    y = plot['y']
+
+    # set x & y axis labels
+    plt.xlabel(x['title'])
+    plt.ylabel(y['title'])
+
+    # actually do the plotting
+    plt.plot(x['values'], y['values'], x['values'], y['values'], 'bo')
 
     # if grid is true, turn on the grid
     if grid:
